e87edc4e77282543db8ec97346fae4f5a3a6cda3
InshaSiS <inshasistechnologies@gmail.com>
2024-02-07 18:56:46 +0530

    feat:POS Setting auto create warehouse

--- rise_pos_connector/rise_pos_connector/doctype/rise_pos_settings/rise_pos_settings.py
+++ rise_pos_connector/rise_pos_connector/doctype/rise_pos_settings/rise_pos_settings.py
@@ -14,21 +14,18 @@ class RisePOSSettings(Document):
 			self.set("licence_no", '')
 			self.set("status", 'Inactive')
 			self.set("shop_code_details", [])
-
+		
 	def before_save(self):
 		if self.enable == 1: 
 			url = "http://dev.onegreendiary.com/erp/get_all_shops"
-
 			# Define the JSON payload
 			payload = {
 				"licence_no": self.licence_no
 			}
-
 			# Specify the API key in the headers
 			headers = {
 				"api_key": self.api_key
 			}
-
 			# Make a POST request
 			response = requests.post(url, json=payload, headers=headers)
 			
@@ -50,31 +47,36 @@ class RisePOSSettings(Document):
 
 @frappe.whitelist()
 def get_all_customers(licence_no,api_key):
-
 	url = "http://dev.onegreendiary.com/erp/get_all_shops"
-
 	# Define the JSON payload
 	payload = {
 		"licence_no": licence_no
 	}
-
 	# Specify the API key in the headers
 	headers = {
 		"api_key": api_key
 	}
-
 	# Make a POST request
 	response = requests.post(url, json=payload, headers=headers)
-
 	if response.status_code == 200:
 		try:
 			# Attempt to parse the JSON content of the response
 			data = response.json()
 			for shop in data['result']['shops']:
-				
 				rps = frappe.get_doc('Rise POS Settings')
-				rps.append('shop_code_details', {'merchant_id': shop['merchant_id'],'shop_code':shop['shop_code'], 'shop_name': shop['name'],'erp_token':shop['erp_token']})
+				rps.append('shop_code_details', {'merchant_id': shop['merchant_id'],'shop_code':shop['shop_code'], 'shop_name': shop['name'], 'shop_phone': shop['shop_phone_number'],'erp_token':shop['erp_token'],'latitude': shop['location']['latitude'],'longitude': shop['location']['longitude'],'address': shop['location']['address']})
 				rps.save(ignore_permissions=True)
+
+				# Create Warehouse
+				wh_list = frappe.get_list('Warehouse', fields=['warehouse_name'])
+				check_wh = {'warehouse_name': shop['name']}
+				if check_wh not in wh_list:
+					warehouse = frappe.get_doc({
+						"doctype": "Warehouse",
+						"warehouse_name": shop['name'],
+						"custom_shop_code": shop['shop_code']
+					})
+					warehouse.insert()
 				
 		except json.JSONDecodeError as e:
 			frappe.msgprint(f"Error decoding JSON: {e}")
